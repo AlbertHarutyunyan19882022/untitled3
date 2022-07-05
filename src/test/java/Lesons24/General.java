59456075a7357e233d4ab8d1f5a29e5643949692
FIRST_NAME LAST_NAME <albert19911988cristina@gmail.com>
2022-07-05 20:18:27 +0300

    ддобавлен комент

--- src/test/java/Lesons24/General.java
+++ src/test/java/Lesons24/General.java
@@ -3,7 +3,7 @@ package Lesons24;
 public class General {
     public static void main(String[] args) {
         sum(1, 3);
-        mult(1,3);
+        mult(1,3);// вызов функции умножение
 
     }
     private static   int sum(int x, int y){
