7410ac71e4ba2fb58b9055af91f29aee5569c836
Vlad3782 <vlad_krasovskij@mail.ru>
2022-07-05 20:01:26 +0300

    Добавлена функция умножения

--- src/test/java/Lesons24/General.java
+++ src/test/java/Lesons24/General.java
@@ -2,10 +2,16 @@ package Lesons24;
 
 public class General {
     public static void main(String[] args) {
-       sum(1,3);
+        sum(1, 3);
+        mult(1,3);
 
     }
     private static   int sum(int x, int y){
         return x+y;
+    }
+
+    private static int mult(int x, int y) {
+        return x * y;
+
     }
 }
